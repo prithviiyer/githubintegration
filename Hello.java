2fb9d6077dddbd6e972c5a553eb86d9d2c26e2c4
prithviiyer <prithvii@vt.edu>
2021-06-08 14:42:19 -0400

    add trippie1

--- Hello.java
+++ Hello.java
@@ -3,6 +3,6 @@ public class Hello {
     public static void main(String args[]) {
         System.out.print("Hello world");
 	System.out.print(" 1");
-	System.out.print(" Trippie");
+	System.out.print(" Trippie1");
     }
 }
